2cf0560b50e0c491f2f9ef5be44dc30432682dc3
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2024-01-14 18:30:15 +0100

    Remove commented out code and unused variables

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// window.onload = function () {
-//   let subjects = document.getElementsByClassName('subject');
-
-//   for (let i = 0; i < subjects.length; i++) {
-//     subjects[i].addEventListener('click', function () {
-//       let timeCell = this.parentNode.getElementsByClassName('time')[0];
-//       let dayCell = document.getElementsByClassName('day')[0];
-//       console.log(dayCell);
-
-//       alert(
-//         `You have ${this.textContent} on ${dayCell.textContent} ${timeCell.textContent}`
-//       );
-//     });
-//   }
-// };
-
 window.onload = function () {
   let subjects = document.getElementsByClassName('subject');
   let days = document.getElementsByClassName('day');
