d6a9edb9f95a1651f1b7750158cbca912b840aa9
Sebastian Patrick Klen <sebastianpatrickklen@gmail.com>
2024-01-14 18:29:50 +0100

    Refactor event listener in script.js to display the correct day when a subject is clicked

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
+// window.onload = function () {
+//   let subjects = document.getElementsByClassName('subject');
+
+//   for (let i = 0; i < subjects.length; i++) {
+//     subjects[i].addEventListener('click', function () {
+//       let timeCell = this.parentNode.getElementsByClassName('time')[0];
+//       let dayCell = document.getElementsByClassName('day')[0];
+//       console.log(dayCell);
+
+//       alert(
+//         `You have ${this.textContent} on ${dayCell.textContent} ${timeCell.textContent}`
+//       );
+//     });
+//   }
+// };
+
 window.onload = function () {
-  var subjects = document.getElementsByClassName('subject');
+  let subjects = document.getElementsByClassName('subject');
+  let days = document.getElementsByClassName('day');
 
-  for (var i = 0; i < subjects.length; i++) {
+  for (let i = 0; i < subjects.length; i++) {
     subjects[i].addEventListener('click', function () {
-      var timeCell = this.parentNode.getElementsByClassName('time')[0];
+      let timeCell = this.parentNode.getElementsByClassName('time')[0];
+      let index = Array.prototype.indexOf.call(this.parentNode.children, this);
+      let dayCell = days[index];
 
-      alert(`You have ${this.textContent} at ${timeCell.textContent}`);
+      alert(
+        `You have ${this.textContent} on ${dayCell.textContent} at ${timeCell.textContent}`.replace(
+          /\s+/g,
+          ' '
+        )
+      );
     });
   }
 };
